b56e814986b4d12ac3fc9abfe55c5f5f01aeebdf
IDSCAN\bmulhall <bmulhall@idscan.net>
2021-02-11 15:20:47 -0600

    just image to our api

--- src/App.js
+++ src/App.js
@@ -4,117 +4,124 @@ import IDVC from '@idscan/idvc'
 import  '@idscan/idvc/dist/css/idvc.css'
 
 class App extends Component {
-    
-    licenseKey = '';
+  constructor(props) {
+    super(props);
+    this.state = {
+      Birthdate: "",
+      Fullname: "",
+      Address1: "",
+      City: "",
+      component: null,
+    };
 
+     this.onInputchange = this.onInputchange.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: false,
-            component: null
-        }
-    }
+  componentWillUnmount() {
+    this.state.component.stopProccesing();
+  }
 
-    componentWillUnmount() {
-        this.state.component.stopProccesing()
-    }
+  componentDidMount() {
+    let _t = this;
+    if (!this.state.component) {
+      this.state.component = new IDVC({
+        networkUrl: "assets/networks",
+        el: "videoCapturingEl",
+        licenseKey:
+          "",
+        types: ["ID"],
+        showSubmitBtn: true,
+        steps: [{ type: "pdf", name: "Back Scan" }],
+        onChange(step) {
+          console.log(step);
+        },
+        onReset(steps) {
+          console.log(steps);
+        },
+        submit(data) {
+          let backStep = data.steps.find((item) => item.type === "pdf");
 
-    componentDidMount () {
-        let _t = this
-        if (!this.state.component) {
-            this.state.component = new IDVC({
-              networkUrl: "networks",
-              el: "videoCapturingEl",
-              licenseKey: "",
-              types: ["ID"],
-              showSubmitBtn: true,
-              steps: [
-                { type: "front", name: "Front Scan" },
-                { type: "back", name: "Back Scan" },
-                { type: "face", name: "Selfie" },
-              ],
-              onChange(step) {
-                console.log(step);
+          let request = {
+            authKey: "",
+            data: backStep.img.split(/:image\/(jpeg|png);base64,/)[2],
+          };
+
+          fetch(
+            "https://app1.idware.net/DriverLicenseParserRest.svc/ParseImage",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json;charset=utf-8",
               },
-              onReset(steps) {
-                console.log(steps);
-              },
-              submit(data) {
-                let backStep = data.steps.find((item) => item.type === "back");
-                let trackString =
-                  backStep && backStep.trackString ? backStep.trackString : "";
+              body: JSON.stringify(request),
+            }
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+              _t.setState({
+                Birthdate: data.ParseImageResult.DriverLicense.Birthdate,
+                Fullname: data.ParseImageResult.DriverLicense.FullName,
+                Address1: data.ParseImageResult.DriverLicense.Address1,
+                City: data.ParseImageResult.DriverLicense.City,
+              });
+            })
+            .catch(() => {});
+        },
+      });
+    }
+  }
 
-                let request = {
-                  frontImageBase64: data.steps
-                    .find((item) => item.type === "front")
-                    .img.split(/:image\/(jpeg|png);base64,/)[2],
-                  backOrSecondImageBase64: backStep.img.split(
-                    /:image\/(jpeg|png);base64,/
-                  )[2],
-                  faceImageBase64: data.steps
-                    .find((item) => item.type === "face")
-                    .img.split(/:image\/(jpeg|png);base64,/)[2],
-                  documentType: data.documentType,
-                  trackString: trackString,
-                };
-                _t.setState({ isLoading: true });
-                fetch("https://dvs2.idware.net/api/Request", {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/json;charset=utf-8",
-                    Authorization: `Bearer `,
-                  },
-                  body: JSON.stringify(request),
-                })
-                  .then((response) => response.json())
-                  .then((response) => {
-                    fetch(
-                      "" + "/api/ValidationRequests/complete/",
-                      {
-                        method: "POST",
-                        headers: {
-                          "Content-Type": "application/json;charset=utf-8",
-                        },
-                        body: JSON.stringify({
-                          requestId: response.requestId,
-                        }),
-                      }
-                    )
-                      .then((response) => response.json())
-                      .then((data) => {
-                        _t.setState({ isLoading: false });
-                        alert(
-                          data.payload.isDocumentSuccess
-                            ? "Document valid"
-                            : "Document invalid"
-                        );
-                      });
-                  })
-                  .catch(() => {
-                    _t.setState({ isLoading: false });
-                  });
-              },
-            });
-        }
-    }
-    
-    render () {
-        const isLoading = this.state.isLoading
-        return (
-            <div>
-                {
-                    !isLoading
-                        ? null
-                        : (
-                            <div className="loading">Verify...</div>
-                        )
-                }
-                <h3>DVS Demo Application</h3>
-                <div id="videoCapturingEl"></div>
-            </div>
-        );
-    }
+  onInputchange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h3>DVS Demo Application</h3>
+          <div id="videoCapturingEl"></div>
+        </div>
+        <div style={{ margin: "50px" }}>
+          <div>
+            <label>Full Name</label>
+            <input
+              type="text"
+              value={this.state.Fullname}
+              onChange={this.onInputchange}
+            />
+          </div>
+          <div>
+            <label>Birth Date</label>
+            <input
+              type="text"
+              value={this.state.Birthdate}
+              onChange={this.onInputchange}
+            />
+          </div>
+          <div>
+            <label> Address</label>
+            <input
+              type="text"
+              value={this.state.Address1}
+              onChange={this.onInputchange}
+            />
+          </div>
+          <div>
+            <label>City</label>
+            <input
+              type="text"
+              value={this.state.City}
+              onChange={this.onInputchange}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
